04b27374788ee44dba83cc26b375148c0a5007c3
Dave Tipping <david.j.tipping@gmail.com>
2018-01-03 16:49:54 +0000

    updated to include power state and on/off
    updated to unclude power on power off and state checking through port 9600.

--- api/controller/sky-remote-api-controller.js
+++ api/controller/sky-remote-api-controller.js
@@ -1,12 +1,40 @@
 'use strict';
 var SkyRemote = require('sky-remote');
+var SkyQ = require('sky-q');
 
 
 exports.send_a_command = function(req, res) {
         var remoteControl = new SkyRemote(req.params.Server);
-        remoteControl.press(req.params.Command,function() {
-                res.end('OK');
-        });
-};
-  
 
+                var box = new SkyQ({ip:req.params.Server})
+                box.getPowerState().then(isOn=>{
+                        if (req.params.Command == "on") {
+                                if(!isOn){
+                                        remoteControl.press("power",function() {
+                                        res.end('OK');
+                                        });
+                                }else{
+                                        res.end('BOX ON');
+                                }
+                        } else if (req.params.Command == "off") {
+                                if(isOn){
+                                        remoteControl.press("power",function() {
+                                        res.end('OK');
+                                        });
+                                }else{
+                                        res.end('BOX OFF');
+                                }
+
+                        } else {
+                                        remoteControl.press(req.params.Command,function() {
+                                        res.end('OK');
+                                        });
+                        }
+                }).catch(err=>{
+                        console.error("Unable to determine power state")
+                        console.error("Perhaps looking at this error will help you figure out why", err)
+                })
+
+
+
+};
